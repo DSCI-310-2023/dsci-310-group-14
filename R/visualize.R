3cb57b32707d83a0def7fecb7b8953985fea2b4f
Berkay Talha Acar <berkaytalhaacar06@gmail.com>
2023-03-11 23:11:40 -0800

    Updated the function to visualize

--- R/visualize.R
+++ R/visualize.R
@@ -1,4 +1,18 @@
-library(ggplot2)
+#' Function to visualize the distributions for features and columns 
+#' 
+#' Display all Player Distributions of Various Players Statistics
+#' 
+#' @param df data frame
+#' @param feature_start columns to start from to visualize
+#' @param feature_end columns to end
+#' @param alpha to reduce overplotting
+#' 
+#' @return NA
+#' 
+#' @examples
+#'visualize_distributions(data_training, feature_start = 3, feature_end = 9, alpha = 4)
+
+
 visualize_distributions <- function(df, feature_start, feature_end, alpha){
   ggpairs(df, columns = feature_start:feature_end, legend = 1,
           ggplot2::aes(color = Is_All_Star, alpha = alpha),
