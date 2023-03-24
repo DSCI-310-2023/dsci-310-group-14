2cb243532eb9aa0080023eb9b698ef84e4796c83
RayNguyent <hoangvnguyen02@gmail.com>
2023-03-24 12:17:31 -0700

    added Makefile

--- src/06_fit.R
+++ src/06_fit.R
@@ -1,11 +1,22 @@
-#' 
-#' Fit the new specification with an optimal k value and the recipe to train 
-#' the model + assess the quality of the model
-#' 
+"Fit the new specification with an optimal k value and the recipe to train 
+ the model + assess the quality of the model
+Usage: src/06_fit.R --training=<training> --test=<test> --recipe=<recipe>  --model=<model>  --outpath=<outpath>
+  
+Options:
+--training=<training> Path (including filename) to training data 
+--test=<test> Path (including filename) to testing data 
+--recipe=<recipe> Path (including filename) to the recipe
+--model=<model> Path (including filename) to the model 
+--outpath=<outpath> Path to directory where the plots and model should be saved
+" -> doc
 library(tidyverse)
 library(GGally)
 library(tidymodels)
 library(themis)
+library(docopt)
+
+opt <- docopt(doc)
+
 main <- function(training,test,recipe, model, outpath) {
   data_training<- read.csv(training)
   data_testing<- read.csv(test)
@@ -58,4 +69,6 @@ main <- function(training,test,recipe, model, outpath) {
   
   ggsave(here::here(paste0(outpath, "/confusion_graph.png")), confusion_graph, width = 5, height = 3)
 }
+
+main(opt[["--training"]], opt[["--test"]],opt[["--recipe"]],opt[["--model"]],opt[["--outpath"]])
 #main(here::here("data/training_set.csv"),here::here("data/testing_set.csv"),here::here("results/data_recipe.rds"),here::here("results/find-k.rds"), "results")
