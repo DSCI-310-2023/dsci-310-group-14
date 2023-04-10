3c699802e5f4f959d3d3093b2b766adacfff6ade
RayNguyent <hoangvnguyen02@gmail.com>
2023-04-10 10:53:32 -0700

    style check for tests + report

--- Tests/test_getallstars.R
+++ Tests/test_getallstars.R
@@ -1,11 +1,10 @@
-
-#install.packages("here")
-#library(here)                    
-#library(testthat)
+# install.packages("here")
+# library(here)
+# library(testthat)
 source(here("R/01_season_filter.R"))
 source(here("R/02_get_all_stars.R"))
 source(here("Tests/testhelper.R"))
 
 test_that("clean_all-stars should return correct data frame", {
-  expect_equivalent(getallstars(2011,2016),testdata)
+  expect_equivalent(getallstars(2011, 2016), testdata)
 })
