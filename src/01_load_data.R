9eacd43701f3ba5a93d7fed3919f385dad37e1b8
RayNguyent <hoangvnguyen02@gmail.com>
2023-04-09 23:02:12 -0700

    style checked + fixed plot size

--- src/01_load_data.R
+++ src/01_load_data.R
@@ -1,25 +1,26 @@
 library(tidyverse)
 
-#read data from repo
+# read data from repo
 players <- read.csv("https://raw.githubusercontent.com/rhysong/dsci-310-group-14/main/data/raw/nba_player_stats.csv")
 all_stars <- read.csv("https://raw.githubusercontent.com/rhysong/dsci-310-group-14/main/data/raw/all_stars_2000_2016.csv")
 
-#filter data for specified years
-all_stars_filtered <- all_stars %>% select(Year, Player) %>% 
+# filter data for specified years
+all_stars_filtered <- all_stars %>%
+  select(Year, Player) %>%
   mutate(Is_All_Star = "All Star")
 
-#combine players data with all_stars data
-combined_data <- left_join(players, all_stars_filtered, by = c("Year", "Player")) %>% 
-  replace(is.na(.), "Regular") %>% 
+# combine players data with all_stars data
+combined_data <- left_join(players, all_stars_filtered, by = c("Year", "Player")) %>%
+  replace(is.na(.), "Regular") %>%
   mutate(Is_All_Star = as_factor(Is_All_Star))
 
 if (!dir.exists("data/processed")) {
   dir.create(here::here("data/processed"))
 }
 if (!dir.exists("results")) {
-    dir.create(here::here("results"))
+  dir.create(here::here("results"))
 }
 
 
-#write combined data to csv
-readr::write_csv(combined_data,here::here(paste0("data/processed", "/nba_allstars.csv")))
+# write combined data to csv
+readr::write_csv(combined_data, here::here(paste0("data/processed", "/nba_allstars.csv")))
--- src/03_summarize.R
+++ src/03_summarize.R
@@ -1,9 +1,9 @@
 doc <- "summarize mean and proportion
 Usage: src/03_summarize.R --outpath=<outpath>
-  
+
 Options:
 --outpath=<outpath> Path to directory where the plots and model should be saved
-" 
+"
 
 
 library(tidyverse)
@@ -12,28 +12,29 @@ library(GGally)
 
 opt <- docopt(doc)
 
-main <- function(outpath){
-  
-#summarize the averages across the variables
-  data<- read.csv(here::here("data/processed/training_set.csv"))
-  averages <- group_by(data, Is_All_Star) %>% 
+main <- function(outpath) {
+  # summarize the averages across the variables
+  data <- read.csv(here::here("data/processed/training_set.csv"))
+  averages <- group_by(data, Is_All_Star) %>%
     summarize(across(MIN:TOV, mean))
-  write_csv(averages,file.path(outpath,"averages.csv"))
-              
+  write_csv(averages, file.path(outpath, "averages.csv"))
+  
+  # calculate the proportion of regular and all-star player
   proportion_summary <- NBASTARS::proportion(data, Is_All_Star)
+  write_csv(proportion_summary, file.path(outpath, "proportion.csv"))
   
-  write_csv(proportion_summary,file.path(outpath,"proportion.csv"))
-  
-  options(repr.plot.width = 14, repr.plot.height = 9) 
-  plot <- ggpairs(data, columns = 3:9, legend = 1,
-                  ggplot2::aes(color = Is_All_Star, alpha = 0.4),
-                  upper = list(continuous = "points", wrap("cor", size = 2.5))) +
+  # plot of every stats pairwise 
+  options(repr.plot.width = 14, repr.plot.height = 9)
+  plot <- ggpairs(data,
+    columns = 3:9, legend = 1,
+    ggplot2::aes(color = Is_All_Star, alpha = 0.4),
+    upper = list(continuous = "points", wrap("cor", size = 2.5))
+  ) +
     labs(title = "Player Distributions of Various Players Statistics", fill = "Players' type") +
     theme(plot.title = element_text(size = 25))
-  
+
   ggsave(here::here(paste0(outpath, "/stats_distributions.png")), plot, width = 11)
-  
 }
 
 main(opt[["--outpath"]])
-#main("results")
+
